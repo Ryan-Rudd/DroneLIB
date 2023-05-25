522dea34205cfbae7c2c79d7ee841af272043e1a
Tech Penguin <131509539+Ryan-Rudd@users.noreply.github.com>
2023-05-25 19:55:10 -0400

    updates test graph and constructor

--- DroneLIB/__test_graph.py
+++ DroneLIB/__test_graph.py
@@ -6,9 +6,9 @@ from droneConstructor import Drone
 drone = Drone()
 
 # Set drone parameters for stability
-drone.mass = 0.5
-drone.thrust_coefficient = 0.8
-drone.drag_coefficient = 0.1
+drone.mass = 12.0001
+drone.thrust_coefficient = 20.8
+drone.drag_coefficient = 50.1
 
 # Set simulation parameters
 total_simulation_time = 10.0  # total simulation time in seconds
@@ -16,28 +16,38 @@ total_simulation_time = 10.0  # total simulation time in seconds
 # Simulate the flight and obtain altitude data
 altitude_data = []
 time_data = []
+roll_stability_data = []
+pitch_stability_data = []
 
 dt = 0.01  # time step size
 num_steps = int(total_simulation_time / dt)
 
 for step in range(num_steps):
-    # Generate random throttle command between 0 and 1
+    # Generate random throttle command between 0.5 and 1.0
     throttle_command = random.uniform(0.5, 1.0)
 
     # Simulate one time step with the random throttle command
     drone.simulate(dt, throttle_command)
 
     # Store altitude and time data
-    altitude_data.append(drone.position[2])
+    altitude_data.append(max(drone.position[2], 0.0))  # Ensure non-negative altitude
     time_data.append(step * dt)
 
-# Plot altitude over time
+    # Analyze stability and store stability data
+    roll_stability, pitch_stability = drone.analyze_stability()
+    roll_stability_data.append(roll_stability)
+    pitch_stability_data.append(pitch_stability)
+
+# Plot altitude, roll stability, and pitch stability over time
 plt.figure(figsize=(10, 5))
-plt.plot(time_data, altitude_data)
+plt.plot(time_data, altitude_data, label='Altitude')
+plt.plot(time_data, roll_stability_data, label='Roll Stability')
+plt.plot(time_data, pitch_stability_data, label='Pitch Stability')
 plt.xlabel('Time (s)')
-plt.ylabel('Altitude (m)')
-plt.title('Drone Altitude over Time')
+plt.ylabel('Value')
+plt.title('Drone Performance')
 plt.grid(True)
+plt.legend()
 plt.show()
 
 # Analyze stability
--- DroneLIB/droneConstructor.py
+++ DroneLIB/droneConstructor.py
@@ -1,16 +1,13 @@
+import random
+import math
 import matplotlib.pyplot as plt
 
 class Drone:
     def __init__(self):
-        # Default parameters
+        # Drone properties
         self.mass = 1.0
         self.thrust_coefficient = 0.1
         self.drag_coefficient = 0.01
-
-        # Drone configuration
-        self.number_of_propellers = 4
-        self.propeller_radius = 0.1
-        self.propeller_thrust = 0.0
 
         # State variables
         self.position = [0.0, 0.0, 0.0]  # [x, y, z]
@@ -26,116 +23,65 @@ class Drone:
         self.pitch = 0.0
         self.yaw_rate = 0.0
 
-        # Battery information
-        self.battery_voltage = 12.0
-        self.battery_capacity = 5000.0
-        self.battery_remaining = 100.0
-
-        # GPS information
-        self.gps_latitude = 0.0
-        self.gps_longitude = 0.0
-        self.gps_altitude = 0.0
-        self.gps_speed = 0.0
-
-        # Camera and sensors
-        self.camera_enabled = False
-        self.camera_resolution = (640, 480)
-        self.imu_enabled = True
-        self.gyroscope_enabled = True
-        self.accelerometer_enabled = True
-
-        # Aerodynamic properties
-        self.wing_area = 0.0
-        self.coefficient_of_lift = 0.0
-        self.coefficient_of_drag = 0.0
-
-        # Motor properties
-        self.motor_max_speed = 1000.0
-        self.motor_min_speed = 0.0
-
-        # Environmental properties
-        self.air_density = 1.225
-
-        # Sensor noise parameters
-        self.accelerometer_noise = 0.0
-        self.gyroscope_noise = 0.0
-
-        # Controller gains
-        self.roll_pid_gain = [0.0, 0.0, 0.0]
-        self.pitch_pid_gain = [0.0, 0.0, 0.0]
-        self.yaw_pid_gain = [0.0, 0.0, 0.0]
-
-        self.MAX_ACCELERATION = 30.0  # Maximum acceleration (m/s^2)
-        self.MAX_VELOCITY = 50.0  # Maximum velocity (m/s)
-
     def apply_thrust(self, throttle):
-        # Ensure throttle is in the range [0, 1]
-        throttle = max(0.0, min(1.0, throttle))
-        self.throttle = throttle
-
         # Compute the thrust produced by each propeller
+        self.throttle = throttle
         self.propeller_thrust = self.thrust_coefficient * self.throttle
-
-        # Compute the total thrust produced by all propellers
-        total_thrust = self.number_of_propellers * self.propeller_thrust
-
-        return total_thrust
 
     def compute_drag(self):
         # Compute the total drag force
-        total_drag = self.drag_coefficient * self.velocity[2] ** 2
-        return total_drag
+        velocity_magnitude = math.sqrt(sum(v ** 2 for v in self.velocity))
+        drag_force = [-self.drag_coefficient * v * velocity_magnitude for v in self.velocity]
+        return drag_force
 
-    def compute_acceleration(self, total_thrust, total_drag):
-        # Compute the net force in the z direction
-        net_force = total_thrust - total_drag - (self.mass * 9.81)
+    def compute_gravity(self):
+        # Compute the gravity force
+        gravity_force = [0.0, 0.0, -self.mass * 9.81]
+        return gravity_force
 
-        # Apply Newton's second law to compute acceleration
-        self.acceleration[2] = net_force / self.mass
+    def compute_net_force(self):
+        # Compute the net force acting on the drone
+        total_thrust = self.propeller_thrust * math.cos(self.pitch) * math.cos(self.roll)
+        total_drag = self.compute_drag()
+        total_gravity = self.compute_gravity()
+
+        net_force = [total_thrust + drag + gravity for drag, gravity in zip(total_drag, total_gravity)]
+        return net_force
+
+    def update_acceleration(self):
+        # Update the acceleration using the current net force
+        self.acceleration = [force / self.mass for force in self.compute_net_force()]
 
     def update_velocity(self, dt):
         # Update the velocity using the current acceleration
-        for i in range(3):
-            self.velocity[i] += self.acceleration[i] * dt
-            # Check if velocity magnitude exceeds maximum
-            if abs(self.velocity[i]) > self.MAX_VELOCITY:
-                self.velocity[i] = self.MAX_VELOCITY if self.velocity[i] > 0 else -self.MAX_VELOCITY
+        self.velocity = [v + a * dt for v, a in zip(self.velocity, self.acceleration)]
 
     def update_position(self, dt):
         # Update the position using the current velocity
-        for i in range(3):
-            self.position[i] += self.velocity[i] * dt
+        self.position = [p + v * dt for p, v in zip(self.position, self.velocity)]
 
     def simulate(self, dt, throttle):
-        # Apply the throttle command
-        total_thrust = self.apply_thrust(throttle)
-
-        # Compute the drag force
-        total_drag = self.compute_drag()
-
-        # Compute the current acceleration
-        self.compute_acceleration(total_thrust, total_drag)
-
-        # Update velocity and position
+        # Simulate the motion of the drone for one time step
+        self.apply_thrust(throttle)
+        self.update_acceleration()
         self.update_velocity(dt)
         self.update_position(dt)
 
-    def simulate_flight(self, time):
+    def simulate_flight(self, time, throttle):
         # Simulate the flight of the drone for the specified time
         dt = 0.01  # time step size
         num_steps = int(time / dt)
         altitude_data = []
-        for step in range(num_steps):
-            self.simulate(dt, self.throttle)
+        for _ in range(num_steps):
+            self.simulate(dt, throttle)
             altitude_data.append(self.position[2])
 
         return altitude_data
 
     def analyze_stability(self):
         # Analyze the stability of the drone
-        # Calculate the roll and pitch stability metrics
-        roll_stability = abs(self.angular_acceleration[0]) / abs(self.angular_velocity[0]) if self.angular_velocity[0] != 0 else float('inf')
-        pitch_stability = abs(self.angular_acceleration[1]) / abs(self.angular_velocity[1]) if self.angular_velocity[1] != 0 else float('inf')
+        roll_stability = abs(sum(self.angular_acceleration) / len(self.angular_acceleration)) / (abs(sum(self.angular_velocity) / len(self.angular_velocity)) + 1e-6)
+        pitch_stability = abs(sum(self.angular_acceleration) / len(self.angular_acceleration)) / (abs(sum(self.angular_velocity) / len(self.angular_velocity)) + 1e-6)
 
         # Determine stability status based on the metrics
         if roll_stability < 1.0 and pitch_stability < 1.0:
@@ -148,8 +94,8 @@ class Drone:
         print(f"Roll Stability: {roll_stability}")
         print(f"Pitch Stability: {pitch_stability}")
         print(f"Stability Status: {stability_status}")
-
-
+        
+        return roll_stability, pitch_stability
 
     def evaluate_performance(self):
         # Evaluate the performance of the drone
